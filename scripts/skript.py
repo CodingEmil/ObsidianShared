48f878ba034e82b3be0fa5f73fc3e0ec453dd1ff
Emil <mail@emilhorstmann.de>
2023-06-21 14:20:22 +0200

    scr

--- scripts/skript.py
+++ scripts/skript.py
@@ -1,1 +1,10 @@
-print("hello")
+import os
+
+def list_files(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            print(os.path.join(root, file))
+
+# Beispielaufruf
+directory_path = "docs/DUP"
+list_files(directory_path)
