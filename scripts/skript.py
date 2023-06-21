e7debcff6ad164d6e0cb00fae8b765bca7293a81
Emil <mail@emilhorstmann.de>
2023-06-21 14:28:48 +0200

    updated script

--- scripts/skript.py
+++ scripts/skript.py
@@ -1,10 +1,22 @@
 import os
 
-def list_files(directory):
+def replace_text_in_files(directory, file_extension, search_text, replace_text):
     for root, dirs, files in os.walk(directory):
         for file in files:
-            print(os.path.join(root, file))
+            if file.endswith(file_extension):
+                file_path = os.path.join(root, file)
+                with open(file_path, 'r') as f:
+                    content = f.read()
+                if search_text in content:
+                    content = content.replace(search_text, replace_text)
+                    with open(file_path, 'w') as f:
+                        f.write(content)
+                        print(f"Text ersetzt in Datei: {file_path}")
 
 # Beispielaufruf
-directory_path = "docs"
-list_files(directory_path)
+directory_path = "/pfad/zum/verzeichnis"
+file_extension = ".md"
+search_text = "title: Ziel-MAC (Punkt-zu-Punkt)\nGibt den nächsten Host an, den ein Paket erreichen soll (Punkt-zu-Punkt)"
+replace_text = "!!! note\ntitle: Ziel-MAC (Punkt-zu-Punkt)\nGibt den nächsten Host an, den ein Paket erreichen soll (Punkt-zu-Punkt)"
+
+replace_text_in_files(directory_path, file_extension, search_text, replace_text)
